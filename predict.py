b58b28852972b803158041b7812f0a0415965b7a
zakaria-lagouader <zakaria.lagouader@gmail.com>
2024-06-23 12:02:20 +0100

    change

--- predict.py
+++ predict.py
@@ -3,9 +3,9 @@ from definitions import *
 from configTrainSaliency01CNN import *
 import trimesh
 
-type = "continuous"
-mesh_name = "data/skeleton_decimated.obj"
-saliency_model = tf.keras.models.load_model("models/model-d-40.h5")
+# type = "continuous"
+mesh_name = "data/vase_decimated.obj"
+saliency_model = tf.keras.models.load_model("models/model-new-20.h5")
 mModel = loadObj(mesh_name)
 updateGeometryAttibutes(mModel, useGuided=useGuided, numOfFacesForGuided=patchSizeGuided, computeDeltas=False,
                         computeAdjacency=False, computeVertexNormals=False)
--- saliencyTrainSaliency01CNN.py
+++ saliencyTrainSaliency01CNN.py
@@ -4,13 +4,14 @@ from configTrainSaliency01CNN import *
 from nets import CNNmodelKeras
 import glob
 import os
-saliency_model=CNNmodelKeras(img_size,num_channels,num_classes,type)
-train_data=[]
-train_labels=[]
-trainSet = sorted([os.path.splitext(os.path.basename(file))[0] for file in glob.glob(rootdir + modelsDir + "*.obj")])[:30]
-print(trainSet)
-for modelName in trainSet:
-    # ======Model information=====================================================================
+import numpy as np
+from multiprocessing import Pool
+
+# Function to process a single model
+def process_model(modelName):
+    train_data = []
+    train_labels = []
+
     mModelSrc = rootdir + modelsDir + modelName + '.obj'
     print(modelName)
     if mode == "MESH":
@@ -22,26 +23,21 @@ for modelName in trainSet:
         V, inds = computePointCloudNormals(mModel, pointcloudnn)
         exportPLYPC(mModel, modelsDir + modelName + '_pcnorm_conf.ply')
 
+    gtdata = np.loadtxt(rootdir + modelsDir + modelName + '.txt', delimiter=',')
 
-    gtdata=np.loadtxt(rootdir + modelsDir + modelName +'.txt', delimiter=',')
-
-    # #saliencyValue=saliencyValue/np.max(saliencyValue)
     print('Saliency ground truth data')
     if type == 'continuous':
         train_labels += gtdata.tolist()
     if type == 'discrete':
         train_labels += [int((num_classes - 1) * s) for s in gtdata.tolist()]
 
-
     if mode == "MESH":
-        iLen = len(mModel.faces)
         patches = [neighboursByFace(mModel, i, numOfElements)[0] for i in range(0, len(mModel.faces))]
-        # Rotation and train data formulation===============================================================================
         for i, p in enumerate(patches):
             if i % 1000 == 0:
-                print(f"{i /100}%")
+                print(f"{i / 100}%")
             patchFacesOriginal = [mModel.faces[i] for i in p]
-            positionsPatchFacesOriginal=np.asarray([pF.centroid for pF in patchFacesOriginal])
+            # positionsPatchFacesOriginal = np.asarray([pF.centroid for pF in patchFacesOriginal])
             normalsPatchFacesOriginal = np.asarray([pF.faceNormal for pF in patchFacesOriginal])
             vec = np.mean(np.asarray([fnm.area * fnm.faceNormal for fnm in patchFacesOriginal]), axis=0)
             vec = vec / np.linalg.norm(vec)
@@ -57,10 +53,9 @@ for modelName in trainSet:
     if mode == "PC":
         iLen = len(mModel.vertices)
         patches = [neighboursByVertex(mModel, i, numOfElements)[0] for i in range(0, len(mModel.vertices))]
-        # patches = np.random.choice(patches, numOfElements, replace=False)
         for i, p in enumerate(patches):
             if i % 1000 == 0:
-                print(f"{i /100}%")
+                print(f"{i / 100}%")
             patchVerticesOriginal = [mModel.vertices[i] for i in p]
             normalsPatchVerticesOriginal = np.asarray([pF.normal for pF in patchVerticesOriginal])
             vec = np.mean(np.asarray([fnm.normal for fnm in patchVerticesOriginal]), axis=0)
@@ -74,30 +69,48 @@ for modelName in trainSet:
                                                                                 HC2I[I2HC[hci, 0], I2HC[hci, 1]]]
             train_data.append((normalsPatchVerticesOriginalR + 1.0 * np.ones(np.shape(normalsPatchVerticesOriginalR))) / 2.0)
 
-# Dataset and labels summarization ========================================================================
-if type == 'continuous':
+    return train_data, train_labels
+
+# Main code
+if __name__ == "__main__":
+    # load model
+    saliency_model = tf.keras.models.load_model(rootdir+sessionsDir+'model-new-20.h5')
+    # saliency_model = CNNmodelKeras(img_size, num_channels, num_classes, type)
+    trainSet = sorted([os.path.splitext(os.path.basename(file))[0] for file in glob.glob(rootdir + modelsDir + "*.obj")])[20:]
+    print(trainSet)
+
+    # Use multiprocessing to process models in parallel
+    with Pool() as pool:
+        results = pool.map(process_model, trainSet)
+
+    # Collect results from the pool
+    train_data = []
+    train_labels = []
+    for data, labels in results:
+        train_data.extend(data)
+        train_labels.extend(labels)
+
+    # Convert train_data and train_labels to numpy arrays
     train_data = np.asarray(train_data)
     train_labels = np.asarray(train_labels)
-    saliency_model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer='adam', metrics=[tf.keras.metrics.RootMeanSquaredError()])
 
+    # Dataset and labels summarization
+    if type == 'continuous':
+        saliency_model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer='adam', metrics=[tf.keras.metrics.RootMeanSquaredError()])
 
-if type == 'discrete':
-    train_data = np.asarray(train_data)
-    train_labels = np.asarray(train_labels)
-    train_labels = to_categorical(train_labels, num_classes=num_classes)
-    saliency_model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
+    if type == 'discrete':
+        train_labels = to_categorical(train_labels, num_classes=num_classes)
+        saliency_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-print(train_data.shape, train_labels.shape)
+    print(train_data.shape, train_labels.shape)
 
-# load model
-# saliency_model = tf.keras.models.load_model(rootdir+sessionsDir+'model-d-20.h5')
+    # Train the model
+    seppoint = int(0.8 * train_data.shape[0])
+    saliency_model.summary()
+    saliency_model_train = saliency_model.fit(x=train_data[:seppoint], y=train_labels[:seppoint], batch_size=batch_size, epochs=120, verbose=1)
+    saliency_model.save(rootdir + sessionsDir + 'model-new-20.h5')
 
-seppoint = int(0.8 * train_data.shape[0])
-saliency_model.summary()
-saliency_model_train = saliency_model.fit(x=train_data[:seppoint], y=train_labels[:seppoint], batch_size=batch_size, epochs=80, verbose=1)
-saliency_model.save( rootdir+sessionsDir +'model-new-20.h5')
-
-# Model evaluation ========================================================================
-loss, acc = saliency_model.evaluate(train_data[seppoint:], train_labels[seppoint:], verbose=0)
-print('Test loss:', loss)
-print('Test accuracy:', acc)
+    # Model evaluation
+    loss, acc = saliency_model.evaluate(train_data[seppoint:], train_labels[seppoint:], verbose=0)
+    print('Test loss:', loss)
+    print('Test accuracy:', acc)
