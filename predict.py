5177152c7352784dcd1acd576f7f95754afe452f
zakaria-lagouader <zakaria.lagouader@gmail.com>
2024-07-02 15:01:07 +0100

    chnages

--- predict.py
+++ predict.py
@@ -4,8 +4,8 @@ from configTrainSaliency01CNN import *
 import trimesh
 
 type = "continuous"
-mesh_name = "data/skull_decimated.obj"
-saliency_model = tf.keras.models.load_model("models/saliency_model-f32.h5")
+mesh_name = "data-1/armadillo_decimated.obj"
+saliency_model = tf.keras.models.load_model("models/saliency_model-f32-not-normalized.h5")
 mModel = loadObj(mesh_name)
 updateGeometryAttibutes(mModel, useGuided=useGuided, numOfFacesForGuided=patchSizeGuided, computeDeltas=False,
                         computeAdjacency=False, computeVertexNormals=False)
@@ -14,24 +14,19 @@ train_data = []
 patches = [neighboursByFace(mModel, i, numOfElements)[0] for i in range(0, len(mModel.faces))]
 # Rotation and train data formulation===============================================================================
 for i, p in enumerate(patches):
-    if i % 1000 == 0:
-        print(f"{i /100}%")
     patchFacesOriginal = [mModel.faces[i] for i in p]
-    positionsPatchFacesOriginal=np.asarray([pF.centroid for pF in patchFacesOriginal])
     normalsPatchFacesOriginal = np.asarray([pF.faceNormal for pF in patchFacesOriginal])
     vec = np.mean(np.asarray([fnm.area * fnm.faceNormal for fnm in patchFacesOriginal]), axis=0)
     vec = vec / np.linalg.norm(vec)
     axis, theta = computeRotation(vec, target)
     normalsPatchFacesOriginal = rotatePatch(normalsPatchFacesOriginal, axis, theta)
-    normalsPatchFacesOriginalR = normalsPatchFacesOriginal.reshape((patchSide, patchSide, 3))
-    if reshapeFunction == "hilbert":
-        for hci in range(np.shape(I2HC)[0]):
-            normalsPatchFacesOriginalR[I2HC[hci, 0], I2HC[hci, 1], :] = normalsPatchFacesOriginal[:,
-                                                                        HC2I[I2HC[hci, 0], I2HC[hci, 1]]]
-    train_data.append((normalsPatchFacesOriginalR + 1.0 * np.ones(np.shape(normalsPatchFacesOriginalR))) / 2.0)
+    # Vectorized Hilbert curve reshaping
+    normalsPatchFacesOriginalR = np.zeros((patchSide, patchSide, 3))
+    normalsPatchFacesOriginalR[I2HC[:, 0], I2HC[:, 1], :] = normalsPatchFacesOriginal.T[HC2I[I2HC[:, 0], I2HC[:, 1]], :]
+    train_data.append((normalsPatchFacesOriginalR + 1.0) / 2.0)
 
-train_data = np.asarray(train_data)
-train_data = (train_data - np.mean(train_data, axis=0)) / np.std(train_data, axis=0)
+train_data = np.asarray(train_data, dtype=np.float32)
+# train_data = (train_data - np.mean(train_data, axis=0)) / np.std(train_data, axis=0)
 predictions = saliency_model.predict(train_data)
 
 if type == "discrete":
